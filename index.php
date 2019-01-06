eaf3bb43ba9a20c38f6d19ab0b0100b169f4e5ff
Dominic Ruettimann <dominic.ruettimann@gmail.com>
2019-01-06 12:42:10 +0100

    added ip to output

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Hello, World! Page</title>
+<title>Hello, World!</title>
 </head>
 <body>
 <?php
-	echo 'Hello, World!';
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else {
+    $ip = $_SERVER['REMOTE_ADDR'];
+}
+echo "Hello " . $ip . "!";
 ?>
 </body>
 </html>
